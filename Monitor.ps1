24135f1958d903cad1459e2e53814de84bfb8446
Ivo Dias <igd753@gmail.com>
2020-01-30 16:20:01 -0300

    Verificar Modulo

--- Monitor.ps1
+++ Monitor.ps1
@@ -1,8 +1,25 @@
-# Verificar se um pool nao esta iniciado
+# Reinicar Pools parados
 # Ivo Dias
 
-# Configuracoes
-Import-Module WebAdministration
+# Verifica se o modulo esta instalado
+function Configurar-Modulo {
+    param (
+        $nomeModulo
+    )
+    # Caso ele exista, faz apenas a importacao
+    if (Get-Module -ListAvailable -Name $nomeModulo) {
+        Write-Host "Importando modulos"
+        Import-Module $nomeModulo -Force
+    } else {
+        Write-Host "Instalando modulos necessarios"
+        Install-Module $nomeModulo -Force
+        Import-Module $nomeModulo -Force
+    }
+}
+
+# Configurando modulos necessarios
+Configurar-Modulo "WebAdministration"
+# Define a localizacao
 set-Location IIS:\AppPools
 
 # Recebe todos os Pools
@@ -10,13 +27,20 @@ $ApplicationPools = Get-ChildItem
 
 # Verifica na lista
 foreach ($AppPool in $ApplicationPools) {
-    $ApplicationPoolName = $AppPool.Name
-    $ApplicationPoolStatus = $AppPool.state
-    Write-Host "$ApplicationPoolName -> $ApplicationPoolStatus"
-
-    if($ApplicationPoolStatus -ne "Started") {
-        Write-Host "-----> $ApplicationPoolName esta parado."
-        Start-WebAppPool -Name $ApplicationPoolName
-        Write-Host "-----> $ApplicationPoolName foi reiniciado."
+    try {
+        $ApplicationPoolName = $AppPool.Name
+        $ApplicationPoolStatus = $AppPool.state
+        Write-Host "$ApplicationPoolName -> $ApplicationPoolStatus"
+    
+        if($ApplicationPoolStatus -ne "Started") {
+            Write-Host "-----> $ApplicationPoolName esta parado."
+            Start-WebAppPool -Name $ApplicationPoolName
+            Write-Host "-----> $ApplicationPoolName foi reiniciado."
+        } 
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message # Recebe o erro
+        Write-Host "Ocorreu um erro ao verificar $ApplicationPoolName"
+        Write-Host "Error: $ErrorMessage"
     }
 }
