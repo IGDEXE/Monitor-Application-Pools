8f944f290ee27c92530742e8da3222f06a4f0b06
Ivo Dias <igd753@outlook.com.br>
2020-01-31 14:33:27 -0300

    AzureRM
    Adicionado suporte ao Azure RM

--- Monitor.ps1
+++ Monitor.ps1
@@ -19,7 +19,7 @@ function Configurar-Modulo {
 
 # Verifica se o IIS esta instalado
 $iisStatus = (Get-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole).State
-if ($iisStatus -eq "Enable") {
+if ($iisStatus -eq "Enabled") {
     Write-Host "IIS esta configurado"
 } else {
     Enable-WindowsOptionalFeature –online –featurename IIS-WebServerRole
@@ -27,6 +27,7 @@ if ($iisStatus -eq "Enable") {
 
 # Configurando modulos necessarios
 Configurar-Modulo "WebAdministration"
+Configurar-Modulo "AzureRM.LogicApp"
 
 # Define a localizacao
 if (Test-Path "IIS:\AppPools") {
@@ -34,6 +35,14 @@ if (Test-Path "IIS:\AppPools") {
 } else {
     Write-Host "Pasta do IIS nao disponivel"
 }
+
+# Faz login no Azure RM
+Connect-AzureRmAccount
+
+# Configuracoes do Azure RM
+$nomeRG = "partsunlimited"
+$nomeLogApp = "Monitor500"
+$nomeTrigger = "RECURRENCE"
 
 # Recebe todos os Pools
 $ApplicationPools = Get-ChildItem
@@ -47,8 +56,16 @@ foreach ($AppPool in $ApplicationPools) {
     
         if($ApplicationPoolStatus -ne "Started") {
             Write-Host "-----> $ApplicationPoolName esta parado."
-            Start-WebAppPool -Name $ApplicationPoolName
-            Write-Host "-----> $ApplicationPoolName foi reiniciado."
+            try {
+                Start-AzureRmLogicApp -ResourceGroupName "$nomeRG" -Name "$nomeLogApp" -TriggerName "$nomeTrigger" # Inicia o Logical App
+                Start-WebAppPool -Name $ApplicationPoolName
+                Write-Host "-----> $ApplicationPoolName foi reiniciado."
+            }
+            catch {
+                $ErrorMessage = $_.Exception.Message # Recebe o erro
+                Write-Host "Ocorreu um erro ao reiniciar o Pool $ApplicationPoolName"
+                Write-Host "Error: $ErrorMessage"
+            }    
         } 
     }
     catch {
